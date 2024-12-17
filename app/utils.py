b7e0b674aff86a691fa2f57e2204cbc3553c69da
RODRIGO TIAGO STEINHORST TRICHES <49219129+steinhorstbr@users.noreply.github.com>
2024-12-17 07:55:08 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -24,17 +24,22 @@ def parse_config():
     return config
 
 def write_config(config):
-    """Salva as configurações no arquivo."""
-    with open(CONFIG_PATH, 'w') as file:
-        for section in config:
-            file.write(f"[{section['name']}]\n")
-            for item in section['items']:
-                if item['type'] == 'note':
-                    file.write(f"### {item['text']}\n")
-                elif item['type'] == 'item':
-                    prefix = '' if item['enabled'] else '#'
-                    file.write(f"{prefix}{item['line']}\n")
-            file.write('\n')
+    """Salva as configurações no arquivo accel-ppp.conf."""
+    try:
+        with open(CONFIG_PATH, 'w') as file:
+            for section in config:
+                file.write(f"[{section['name']}]\n")
+                for item in section['items']:
+                    if item['type'] == 'note':
+                        file.write(f"### {item['text']}\n")
+                    elif item['type'] == 'item':
+                        prefix = '' if item['enabled'] else '#'
+                        file.write(f"{prefix}{item['line']}\n")
+                file.write('\n')
+        print("Configurações salvas com sucesso.")
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo: {e}")
+        raise
 
 def create_backup(config_path):
     """Cria um backup antes de salvar as alterações."""
