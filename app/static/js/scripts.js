4dd3b15e071a830c63bf35d0a010ec1766891a2a
RODRIGO TIAGO STEINHORST TRICHES <49219129+steinhorstbr@users.noreply.github.com>
2024-12-17 09:22:36 -0400

    Update scripts.js

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
         .catch(error => alert("Erro ao carregar configurações: " + error));
 });
 
-// Função para renderizar as seções na página
 function populateSections(config) {
     let sections = $("#sections");
     sections.empty();
